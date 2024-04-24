284fff46b37085be91a53b12296a0f4669c4287b
BCCWSIM <167340173+BCCWSIM@users.noreply.github.com>
2024-04-24 13:47:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -225,7 +225,7 @@ exportButton.addEventListener('click', exportCSV);
 const clearSelectionButton = document.getElementById('clearSelectionButton');
 clearSelectionButton.addEventListener('click', clearSelection);
 
-let isTableView = true;
+let isTableView = false;
 
 document.getElementById('toggleViewButton').addEventListener('click', toggleView);
 
@@ -241,6 +241,7 @@ function toggleView() {
         displayGallery(items);
     }
 }
+toggleView();
 
 function displayGallery(data) {
     const gallery = document.getElementById('csvGallery');
