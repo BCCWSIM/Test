ef523d9edbda675c9d41f96044fc1b6b85a8c6ec
BCCWSIM <167340173+BCCWSIM@users.noreply.github.com>
2024-04-29 14:44:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 window.onbeforeunload = () => window.scrollTo(0, 0);
+
+// Store DOM elements in variables
+var uniqueCodeElement = document.getElementById('uniqueCode');
+var firstTabLink = document.getElementsByClassName("tablink")[0];
+
 window.onload = function() {
   toggleView();
+
+  // Generate unique code
   var uniqueCode = new Date().getTime();
   uniqueCode = String(uniqueCode).substr(-5); // get the last 5 digits
-  document.getElementById('uniqueCode').textContent = uniqueCode;
 
-  // Simulate a click on the first tab
-  document.getElementsByClassName("tablink")[0].click();
+  // Update DOM elements
+  uniqueCodeElement.textContent = uniqueCode;
+  firstTabLink.click(); // Simulate a click on the first tab
 }
 
 let items = [];
